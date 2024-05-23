ce4a286000945f0c37460b9ccd93c91527aa2374
Sai-rohit17 <dsrohit2000@gmail.com>
2024-05-23 21:50:31 +0530

    added trailer video and fetched data in the browse

---
+++ src/components/SecondaryContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SecondaryContainer = () => {
+  return (
+    <div>
+      <p>supp</p>
+    </div>
+  )
+}
+
+export default SecondaryContainer
---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const USER_AVATAR = "https://example.com/jane-q-user/profile.jpg";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDVjYTk3N2ViNzNjMmM1NmMxOTYzYjI3Zjg1ODc3OSIsInN1YiI6IjY2NDRmNGZkNmRlZjIzZjk5YjI0MWJhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EEFJAmYBj1Sc_P0-VVILU54qltlIyw-CaJ50eG_HA1Y'
+    }
+  };
+  
